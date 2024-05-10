11616a38e2563dc27898de56ea69348b6535cf4a
bafanamojapelo <bafana.mojapelo@gmail.com>
2024-05-10 11:39:27 +0200

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,19 +1,19 @@
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
-from pydantic.types import conint
-from app.models import Post
-from pydantic import conint
 
+from pydantic.types import conint
 
 
 class PostBase(BaseModel):
     title: str
     content: str
-    published: bool= True
+    published: bool = True
+
 
 class PostCreate(PostBase):
     pass
+
 
 class UserOut(BaseModel):
     id: int
@@ -23,45 +23,44 @@ class UserOut(BaseModel):
     class Config:
         orm_mode = True
 
+
 class Post(PostBase):
     id: int
     created_at: datetime
     owner_id: int
     owner: UserOut
-    title: str
-    content: str
 
     class Config:
         orm_mode = True
 
-class PostOut(PostBase):
+
+class PostOut(BaseModel):
     Post: Post
     votes: int
 
     class Config:
         orm_mode = True
 
+
 class UserCreate(BaseModel):
     email: EmailStr
-    password : str
+    password: str
 
 
 class UserLogin(BaseModel):
-     email: EmailStr
-     password: str
-    
+    email: EmailStr
+    password: str
+
+
 class Token(BaseModel):
     access_token: str
     token_type: str
 
+
 class TokenData(BaseModel):
     id: Optional[str] = None
-    
+
 
 class Vote(BaseModel):
     post_id: int
     dir: conint(le=1)
-
-
-
-    
