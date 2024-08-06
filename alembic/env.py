15cb492021f43e94acc803a453d031f89d504741
bafanamojapelo <bafana.mojapelo@gmail.com>
2024-08-06 13:35:19 +0200

    Updated import for BaseSettings to pydantic-settings

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,16 @@ from alembic import context
 from app.models import Base
 from app.config import settings
 
+from sqlalchemy import engine_from_config
+
+config = context.config
+connectable = engine_from_config(
+    config.get_section(config.config_ini_section),
+    prefix="sqlalchemy.",
+    poolclass=pool.NullPool,
+)
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
