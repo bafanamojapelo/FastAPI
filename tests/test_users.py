787312941cef29d03d4e9e43d2314af834a1b294
bafanamojapelo <bafana.mojapelo@gmail.com>
2024-09-08 08:41:05 +0200

    Your commit message

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,9 +1,16 @@
 from fastapi.testclient import TestClient
 from app.main import app
 
-
 client = TestClient(app)
 
-def test_root():
+def test_root(): 
     res = client.get("/")
-    print(res)
+    print(res.json().get('message'))  # Print the actual response content
+    assert res.json().get('message') == 'Hello World'
+    assert res.status_code == 200
+
+
+def test_create_user():
+    res = client.post("/users/", json={"email": "hello123@gmail.com", "password":"password123"})
+    print(res.json())
+    assert res.status_code == 201
