5d2b9c1827b764b8b45282c17cf2b3210c950aa5
bafanamojapelo <bafana.mojapelo@gmail.com>
2024-09-11 23:02:52 +0200

    Your commit message

--- tests/conftest.py
+++ tests/conftest.py
@@ -38,3 +38,14 @@ def client(session):
             session.close()
     app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
+
+    # Fixture to create a test user
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "bafana@gmail.com", "password": "password123"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
