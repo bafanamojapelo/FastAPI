53ee71c7c83792733f17876303683b0f7cab2555
bafanamojapelo <bafana.mojapelo@gmail.com>
2024-10-07 21:22:38 +0200

    added database

--- tests/database.py
+++ tests/database.py
@@ -2,26 +2,31 @@ from fastapi.testclient import TestClient
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 from app.main import app
-
 from app.config import settings
-from app.database import get_db
-from app.database import Base
+from app.database import get_db, Base
+from alembic.config import Config
 from alembic import command
 
-# SQLACHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost:5432/fastapi_test'
+# Database URL for the test environment
 SQLACHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
-
+# Create a new engine for the test database
 engine = create_engine(SQLACHEMY_DATABASE_URL)
-
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Path to Alembic's configuration file
+alembic_cfg = Config("alembic.ini")
 
 @pytest.fixture()
 def session():
+    # Drop all tables before each test
     Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
+
+    # Apply migrations using Alembic
+    command.upgrade(alembic_cfg, "head")
+
+    # Create a new session for the test database
     db = TestingSessionLocal()
     try:
         yield db
@@ -30,11 +35,12 @@ def session():
 
 @pytest.fixture()
 def client(session):
+    # Override the dependency with the test session
     def override_get_db():
-
         try:
             yield session
         finally:
             session.close()
+
     app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
